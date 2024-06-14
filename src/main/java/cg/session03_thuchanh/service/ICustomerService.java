56bf7b0242fc7f957b32db371c1ab4781cfff495
PhamQuang123 <phamtienquang57@gmail.com>
2024-06-14 08:36:36 +0700

    init upload

---
+++ src/main/java/cg/session03_thuchanh/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package cg.session03_thuchanh.service;
+
+import cg.session03_thuchanh.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
